8dfe99bfd9a6a583dcbecdc9eb45e141cad9d1bd
mynameSumin <msm4167@naver.com>
2023-11-10 16:06:34 +0900

    build 완료

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { io } from "socket.io-client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  //연결 확인 부분
   const socket = io.connect("http://localhost:3001");
+  const [audio, setAudio] = useState(); //소켓에서 데이터 수신
   const sendMessage = (err) => {
     if (err) {
       alert(err);
     } else {
-      socket.emit("send_message", { message: "hi" });
-      console.log("성공");
+      socket.emit("send_message", "hi");
+      console.log("서버 연결 성공");
     }
   };
+
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+  useEffect(() => {
+    socket.on("audio", (data) => {
+      const binaryData = atob(data);
+      const arrayBuffer = new ArrayBuffer(binaryData.length);
+      const byteArray = new Uint8Array(arrayBuffer);
+      for (let i = 0; i < binaryData.length; i++) {
+        byteArray[i] = binaryData.charCodeAt(i);
+      }
+      audioContext.decodeAudioData(arrayBuffer, function (buffer) {
+        const source = audioContext.createBufferSource();
+        source.buffer = buffer;
+        source.connect(audioContext.destination);
+        source.start(0);
+      });
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -23,6 +44,15 @@ function App() {
         }}
       >
         서버 연결 확인
+      </button>
+      <button
+        onClick={() => {
+          audioContext.resume().then(() => {
+            console.log(audioContext.state);
+          });
+        }}
+      >
+        합주시작
       </button>
     </div>
   );
